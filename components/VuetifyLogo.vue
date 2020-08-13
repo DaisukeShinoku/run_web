45ad7be66a9d428cc107b9020cedbe5a399f7120
DaisukeShinoku <shinoku.daisuke.ycu@gmail.com>
2020-08-13 16:42:42 +0900

    setting for firebase

--- components/VuetifyLogo.vue
+++ components/VuetifyLogo.vue
@@ -1,22 +1,15 @@
 <template>
-  <img
-    class="VuetifyLogo"
-    alt="Vuetify Logo"
-    src="/vuetify-logo.svg"
-  >
+<div>
+hello
+</div>
 </template>
 
-<style>
-.VuetifyLogo {
-  height: 180px;
-  width: 180px;
-  transform: rotateY(560deg);
-  animation: turn 3.5s ease-out forwards 1s;
-}
-
-@keyframes turn {
-  100% {
-    transform: rotateY(0deg);
+<script>
+export default {
+  components: {
+  },
+  created () {
+    console.log('API_KEY:', process.env.API_KEY)
   }
 }
-</style>
+</script>
